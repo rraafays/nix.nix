c933f28b47b649d62b98620b6367fcf8735824a7
raf <rraf@tuta.io>
2024-11-05 07:40:00 +0000

    update: migrated nix package manager configuration to separate module

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+{
+  nix = {
+    optimise = {
+      dates = [ "daily" ];
+      automatic = true;
+    };
+    gc = {
+      dates = "daily";
+      options = "--delete-older-than 5d";
+      automatic = true;
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
+}
